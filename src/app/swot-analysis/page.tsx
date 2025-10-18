3e6eaa2f4c6d0e478ec6c257d7852427328298c9
John Finch <jonattfin@gmail.com>
2025-10-19 00:01:35 +0300

    feat: add swot data

--- src/app/swot-analysis/page.tsx
+++ src/app/swot-analysis/page.tsx
@@ -1,10 +1,11 @@
-import {Container, Input, InputGroup, Kbd, Card, Image, Grid, Center} from "@chakra-ui/react";
+import {Container, Input, InputGroup, Kbd, Card, Image, Grid, Center, Blockquote} from "@chakra-ui/react";
 import {LuSearch} from "react-icons/lu";
 
 type SwotCardItem = {
   name: string;
   imageSrc: string;
   description: string;
+  data?: string[],
 }
 
 const cardItems: SwotCardItem[] = [
@@ -12,21 +13,41 @@ const cardItems: SwotCardItem[] = [
     name: "Strengths",
     imageSrc: "https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3",
     description: "Characteristics of the business or project that give it an advantage over others",
+    data: [
+      "Brand loyalty and reputation",
+      "Easy-to-use software products",
+      "Strong distribution channels"
+    ]
   },
   {
     name: "Weaknesses",
     imageSrc: "https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3",
     description: "Characteristics that place the business or project at a disadvantage relative to others.",
+    data: [
+      "Dependence on hardware manufacturers",
+      "Past poor acquisitions",
+      "Security flaws criticism"
+    ]
   },
   {
     name: "Opportunities",
     imageSrc: "https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3",
     description: "Elements in the environment that the business or project could exploit to its advantage.",
+    data: [
+      "Cloud infrastructure growth",
+      "AI innovation and new products",
+      "Growth in emerging markets"
+    ]
   },
   {
     name: "Threats",
     imageSrc: "https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3",
     description: "Elements in the environment that could cause trouble for the business or project.",
+    data: [
+      "Intense competition (AWS, Google)",
+      "Rapid technological changes",
+      "Cybersecurity risks"
+    ]
   }
 ]
 
@@ -57,6 +78,17 @@ function SwotCard(card: SwotCardItem) {
         <Card.Title>{card.name}</Card.Title>
         <Card.Description>
           {card.description}
+          <div>&nbsp;</div>
+          {card.data?.map(d => <div key={d}>
+            <>
+              <Blockquote.Root colorPalette={"yellow"}>
+                <Blockquote.Content>
+                  {d}
+                </Blockquote.Content>
+              </Blockquote.Root>
+              <div>&nbsp;</div>
+            </>
+          </div>)}
         </Card.Description>
       </Card.Body>
     </Card.Root>
